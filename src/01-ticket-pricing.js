765bf8a5c63c799ef6fadcfeec5d920f76ba93b4
Ravi Chauhan <ghost96@Ravis-MacBook-Air.local>
2026-06-02 21:06:52 +0530

    feat: add ticket pricing system

--- src/01-ticket-pricing.js
+++ src/01-ticket-pricing.js
@@ -24,4 +24,17 @@
  */
 export function getTicketPrice(age, isWeekend) {
   // Your code here
+  if(age< 0 ) return -1 
+
+  let basePrice = 0;
+  
+  if(age <= 12) basePrice = 8
+  else if( age <= 17) basePrice = 12
+  else if(age <= 59) basePrice = 15
+  else basePrice = 10
+
+
+  return isWeekend? basePrice+3 : basePrice
 }
+
+
